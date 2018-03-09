def2a75f81d1828036a8f57a6a2bf718b0ce2b2a
antonybaciliosalazar@gmail.com <antonybaciliosalazar@gmail.com>
2018-03-09 01:45:27 +0100

    head fonctions

---
+++ solution.h
@@ -0,0 +1,6 @@
+int *recherche_local(int *tab,int *tmin,int *tmax,int **mt,int nb);
+int *insert(int *tmp,int ins_nub,int nb);
+int evaluer(int *tab,int *tmin,int *tmax,int **mt,int nb);
+int *transfer(int *destination,int *sortie,int nb);
+void affiche(int *tab,int nb);
+int *glouton_fonct(int *tmin,int *tmax,int **mt,int nb);
